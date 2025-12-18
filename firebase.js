1d5a480aeab15113b0fdb348d78c84bf7f8041fb
sweet-potato765 <renga1232@gmail.com>
2025-12-19 08:53:37 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,28 +1,28 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase 설정
 const firebaseConfig = {
   apiKey: "AIzaSyAqQ7R8HRu5ZyoL9nvwTfFj2CNQzhVsTRk",
   authDomain: "cooking-community-bf26e.firebaseapp.com",
   projectId: "cooking-community-bf26e",
-  storageBucket: "cooking-community-bf26e.firebasestorage.app",
+  storageBucket: "cooking-community-bf26e.appspot.com",
   messagingSenderId: "703243645056",
-  appId: "1:703243645056:web:43592fc755b30c4464b665",
-  measurementId: "G-8XHR9RFRJ8"
+  appId: "1:703243645056:web:43592fc755b30c4464b665"
 };
 
-// Initialize Firebase
+// Firebase 초기화
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
 
+// 로그아웃
 window.logout = () => {
-  signOut(auth).then(() => {
-    alert("로그아웃 되었습니다");
-    location.href = "login.html";
-  });
-
+  signOut(auth)
+    .then(() => {
+      alert("로그아웃 되었습니다");
+      location.href = "login.html";
+    })
+    .catch((err) => {
+      alert(err.message);
+    });
 };
